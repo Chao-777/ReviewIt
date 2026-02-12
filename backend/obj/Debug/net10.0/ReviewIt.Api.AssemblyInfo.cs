e937cf056de93f97906fd1d8dae0801fc2aa5719
Chao <nicyuan333@gmail.com>
2026-02-12 12:05:17 +1000

    Fixed user whodoes not have a number cannot login

--- backend/obj/Debug/net10.0/ReviewIt.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/ReviewIt.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReviewIt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a277cbbd3a1ffcdd5602eb50dbc8dcc3aad1e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df578ad6c869e1da25c978d8b25541b0487b7ec2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReviewIt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReviewIt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
